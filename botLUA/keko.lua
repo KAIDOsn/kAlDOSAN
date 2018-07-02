4e4603f377ed43d9d817d07e6921754ce420a136
KAIDOsn <40748382+KAIDOsn@users.noreply.github.com>
2018-07-01 20:31:05 -0700

    Update keko.lua

--- botLUA/keko.lua
+++ botLUA/keko.lua
@@ -1,29 +1,31 @@
--- BY : @ikeko My file :@file_lua
+-- BY : @KAIDOsn
 local action = function(msg, matches)
  if matches[1] == "start" then
 			keyboard = {} 
 	keyboard.inline_keyboard = {
       {
-		{text = "قنات السورس", url="https://t.me/botlua"},					
+		{text = "🔰 قناة السورس", url="https://t.me/teem_kaido"},					
 			},	
 		     {
-		{text = "شرح سورس عله يوتيوب", url="https://youtu.be/W_hTq5Rrk_o"},					
+		{text = "شرح سورس عله التلكرام", url="https://t.me/teem_kaido"},					
 			},
        {
-		{text = "مبرمج السورس", url="https://t.me/ikeko"},					
+		{text = "📊 مطور السورس 📊", url="https://t.me/KAIDOsn"},					
 			},	
 	}
-		api.sendKeyboard(msg.chat.id, '• مرحبا بك 👋🏻ֆ\n• في بوت تشغيل ملفات\n• الخاصه بـ @FILE_LUA\n• فقط ارسل الملف وسأقوم بتشغيله ✅ֆ', keyboard)     
+		api.sendKeyboard(msg.chat.id, '🏷 : اهلا بك عزيزي ارسل الملف وساقوم انا بتشغيله 
+
+🔰: قناة الملفات * @ON_BOTS_LUA *', keyboard)     
 	end	
 	    if msg.document then				
-			api.sendMessage(msg.chat.id, 'نتضر حتا يتم رفع ملف')
+			api.sendMessage(msg.chat.id, '💈: انتظر لكي يتم تشغيل الملف')
       local url = 'https://api.telegram.org/bot'..config.bot_api_key..'/getFile?file_id='..(msg.document.file_id or msg.forward_from.document.file_id)..''
 			local res = HTTPS.request(url)
 			local jres = JSON.decode(res)	
-			local download = download_to_file("https://api.telegram.org/file/bot"..config.bot_api_key.."/"..jres.result.file_path, 'botLUA/keko.lua')
-			api.sendMessage(msg.chat.id, 'تم رفع الملف')
-    	api.sendMessage(msg.chat.id, 'جاري اعاده تشغيل البوت')
-    	api.sendMessage(msg.chat.id, 'تم اعاده تشغيل وتشغيل الملف المطلوب بنجاح')
+			local download = download_to_file("https://api.telegram.org/file/bot"..config.bot_api_key.."/"..jres.result.file_path, 'botLUA/kaido.lua')
+			api.sendMessage(msg.chat.id, '📊: تم تشغيل الملف')
+    	api.sendMessage(msg.chat.id, '🔋: جار تشغيل البوت')
+    	api.sendMessage(msg.chat.id, '💎: تم اكتمال تشغيل البوت')
 		  bot_init(true)
   end
 	end
